57f9240192e198c2338c697f6ae08ba21dfebc75
abdomostafa <https://github.com/Abdo-Mostafa2458/flutter_quran_app.git>
2024-08-29 10:19:19 +0300

    +update the revers number problem

--- bin/revres_number.dart
+++ bin/revres_number.dart
@@ -1,14 +1,22 @@
 
-import 'dart:io';
 
 void main() {
-  print('enter the number ');
-  int number = int.parse(stdin.readLineSync()); //423
-  int digit_1=0,digit_2=0 ,revers=0;
-  digit_1 = number % 10;//3
-  number= number ~/10;//42
-  digit_2 = number % 10;//2
-  number= number ~/10;//4
-  revers = (digit_1*100)+(digit_2*10)+number;
-  print(revers);
+  // print('enter the number ');
+  // int number = int.parse(stdin.readLineSync()); //423
+  // int digit_1=0,digit_2=0 ,revers=0;
+  // digit_1 = number % 10;//3
+  // number= number ~/10;//42
+  // digit_2 = number % 10;//2
+  // number= number ~/10;//4
+  // revers = (digit_1*100)+(digit_2*10)+number;
+  // print(revers);
+   int number = 1234;
+   int revers =0;
+   while(number>0){//t
+     int digit = number%10; // 4 , 3
+       revers = (revers * 10)+digit ;
+
+     number=number~/10;//123
+   }
+   print(revers);
 }
